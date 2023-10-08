202504e322deec2fcd2da3144147284d840e5615
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-08 17:42:53 -0400

    added scrolling to about page

--- app/components/AboutRightContainer.tsx
+++ app/components/AboutRightContainer.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 // Define the prop type for selectedItem
 type AboutRightContainerProps = {
   selectedItem: string;
 };
 
-const AboutRightContainer: React.FC<AboutRightContainerProps> = ({ selectedItem }) => {
+const AboutRightContainer: React.FC<AboutRightContainerProps> = ({
+  selectedItem,
+}) => {
   let content = "";
 
   if (selectedItem === "bio") {
     content = `
-      lorem ipsum dolor sit amet, consectetur adip. lore lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum d lorem ipsum dolor sit amet lorem ipsum d lr     lorem ipsum d lorem ipsum d lorem ipslorem ipsum d lorem ipsum d lore lorem legend lorem ipsum d lorem ipsum d lorem ips
+    Lori lived her life through the lens of a camera. She never realized this until this very moment as she scrolled through thousands of images on your computer. She could remember the exact moment each photo was taken. She could remember where she had been, what she was thinking as she tried to get the shot, the smells of the surrounding area, and even the emotions that she felt taking the photo, yet she had trouble remembering what she had for breakfast.
+The trees, therefore, must be such old and primitive techniques that they thought nothing of them, deeming them so inconsequential that even savages like us would know of them and not be suspicious. At that, they probably didn't have too much time after they detected us orbiting and intending to land. And if that were true, there could be only one place where their civilization was hidden.
+The wave crashed and hit the sandcastle head-on. The sandcastle began to melt under the waves force and as the wave receded, half the sandcastle was gone. The next wave hit, not quite as strong, but still managed to cover the remains of the sandcastle and take more of it away. The third wave, a big one, crashed over the sandcastle completely covering and engulfing it. When it receded, there was no trace the sandcastle ever existed and hours of hard work disappeared forever.
+I recently discovered I could make fudge with just chocolate chips, sweetened condensed milk, vanilla extract, and a thick pot on slow heat. I tried it with dark chocolate chunks and I tried it with semi-sweet chocolate chips. It's better with both kinds. It comes out pretty bad with just the dark chocolate. The best add-ins are crushed almonds and marshmallows -- what you get from that is Rocky Road. It takes about twenty minutes from start to fridge, and then it takes about six months to work off the twenty pounds you gain from eating it. All things in moderation, friends. All things in moderation.
+
+  
     `;
   } else if (selectedItem === "interests") {
     content = "This is my interests for now.";
@@ -22,11 +30,12 @@ const AboutRightContainer: React.FC<AboutRightContainerProps> = ({ selectedItem 
   return (
     <section className="md:flex w-full">
       {/* LEFT SPLIT */}
-      <section className="md:flex-[50%] flex flex-col">
+
+      <section className="md:flex-[50%] flex flex-col relative">
         {/* TOPSIDE DESKTOP */}
         <section className="link-color hidden md:flex items-center border-b border-r border-color">
-          <div className="flex items-center gap-6 px-4 h-full py-[4.20px] border-r border-color ">
-            personal-info
+          <div className="flex items-center gap-6 px-4 h-full py-[4.20px] border-r border-color">
+            {`${selectedItem}`}
             <AiOutlineClose size={16} />
           </div>
         </section>
@@ -42,13 +51,25 @@ const AboutRightContainer: React.FC<AboutRightContainerProps> = ({ selectedItem 
         </section>
 
         {/* CONTENT */}
-        <section className="md:flex-[50%] mt-7 md:mt-4 px-4">
+        <ScrollArea
+          className="md:flex-[50%] mt-7 md:mt-4 px-4 max-h-[310px] md:max-h-[610px] md:overflow-y-hidden"
+          style={{
+            overflowY: "auto", // Show the scrollbar by default
+          }}
+        >
           <h1 className="text-[14px] md:text-[15px]">{content}</h1>
-        </section>
+        </ScrollArea>
       </section>
 
       {/* RIGHT SPLIT */}
-      <section className="md:flex-[50%]">2</section>
+      <section className="md:flex-[50%] border-l border-color">
+        {/* TOPSIDE DESKTOP */}
+        <section className="link-color hidden md:flex items-center border-b border-color">
+          <div className="flex items-center gap-6 px-4 h-full py-[4.20px] text-[#011627]">
+            1
+          </div>
+        </section>
+      </section>
     </section>
   );
 };
