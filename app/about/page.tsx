1296b75e535edc898f85e6846f420278e8e43841
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-07 23:49:10 -0400

    added about page HARD

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
 
 const about = () => {
   return (
-    <main className="relative w-full h-screen border border-color rounded-xl bg-[#011627] px-4">
+    <main className="relative w-full h-screen border border-color rounded-xl bg-[#011627] ">
       {/* HEADER */}
 <div className="absolute left-0 w-full px-4 border-b border-color md:px-14 xl:px-32 2xl:px-60 z-40 "><Navbar /></div>
     
     {/* CONTAINER */}
-    <section>
+    <section className='w-full h-full '>
+      <div className="pt-[49px] pb-[41px] w-full h-full flex">
+        <div className="absolute w-full border-b border-color flex">
+          
+          
+        </div>
+        <Sidebar />
+
+      </div>
       
     </section>
 
---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,80 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside className='md:flex w-full text-white z-10'>
+        
+        {/* LEFT SIDE */}
+      <section className="flex-[20%] ">
+
+        {/* SMALL MENU ITEM */}
+        <section className='flex items-center gap-2 py-1 pl-4 bg-gray-800'>
+        <Image
+      src="/DownArrowFilled.png"
+      width={12}
+      height={12}
+      alt="Picture of the author"
+    />
+            personal-info
+        </section>
+
+        {/* ITEMS 1 */}
+        <div className="">
+
+            <div className="flex items-center pl-4 py-1">
+            <Image className='mb-1 mr-3'
+      src="/ArrowRightOl.png"
+      width={10}
+      height={10}
+      alt="Picture of the author"
+    />
+                <Image className='mb-1 mr-2'
+      src="/OrangeFolder.png"
+      width={15}
+      height={15}
+      alt="Picture of the author"
+    />
+    bio
+            </div>
+        </div>
+
+         {/* ITEMS 2 */}
+         <div className="mt-1">
+
+<div className="flex items-center pl-4 py-1 text-[#607B96]">
+<Image className='mb-1 mr-3'
+src="/ArrowRightOl.png"
+width={10}
+height={10}
+alt="Picture of the author"
+/>
+    <Image className='mb-1 mr-2'
+src="/GreenFolder.png"
+width={15}
+height={15}
+alt="Picture of the author"
+/>
+interests
+</div>
+</div>
+
+
+
+
+      </section>
+
+        {/* RIGHT SIDE */}
+      <section className="flex-[80%] md:flex">
+        {/* LEFT SPLIT */}
+        <div className="flex-[50%] ">left split</div>
+
+         {/* RIGHT SPLIT */}
+         <div className="flex-[50%] ">right split</div>
+      </section>
+
+    </aside>
+  )
+}
+
+export default Sidebar
