77d10df8cf07585160eb33c585f00c2078e9a445
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-09 21:26:53 -0400

    added tempeory rock paper scissors game and fixed homescreen might revert

--- app/components/Comingsoon.tsx
+++ app/components/Comingsoon.tsx
@@ -1,19 +1,146 @@
 'use client'
 
-import React from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { FaHandRock, FaHandPaper, FaHandScissors } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
 
-const Comingsoon = () => {
-    const [items, setItems] = useState([
-        
-    ])
-
-  return (
-    <div className="bg-white/5 rounded-xl w-full h-[175px] md:h-[300px] lg:h-[400px]">
-    <p className='text-base lg:text-4xl text-gray-300 flex items-center justify-center h-full'>coming soon</p>
-    </div>
-  )
+enum Choice {
+  Rock = 'rock',
+  Paper = 'paper',
+  Scissors = 'scissors',
 }
 
-export default Comingsoon
+const GameChoices = [Choice.Rock, Choice.Paper, Choice.Scissors];
+const RoundsToWin = 3;
 
+const Comingsoon = () => {
+  const [userChoice, setUserChoice] = useState<string | null>(null);
+  const [computerChoice, setComputerChoice] = useState<string | null>(null);
+  const [result, setResult] = useState<string | null>(null);
+  const [roundsPlayed, setRoundsPlayed] = useState(0);
+  const [userWins, setUserWins] = useState(0);
+  const [computerWins, setComputerWins] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  useEffect(() => {
+    if (roundsPlayed === RoundsToWin) {
+      if (userWins > computerWins) {
+        setResult('You won the game! 🎉 Let\'s chat over coffee!');
+      } else if (computerWins > userWins) {
+        setResult('Kamel won the game. Try again!');
+      } else {
+        setResult('It\'s a tie game. Try again!');
+      }
+    }
+  }, [roundsPlayed, userWins, computerWins]);
+
+  const startGame = () => {
+    setGameStarted(true);
+    setRoundsPlayed(0);
+    setUserWins(0);
+    setComputerWins(0);
+    setResult(null);
+  };
+
+  const handleChoice = (choice: string) => {
+    if (!loading && gameStarted) {
+      const computerRandomChoice = GameChoices[Math.floor(Math.random() * GameChoices.length)];
+      setUserChoice(choice);
+      setComputerChoice(computerRandomChoice);
+
+      const winConditions: Record<string, string> = {
+        [Choice.Rock]: Choice.Scissors,
+        [Choice.Paper]: Choice.Rock,
+        [Choice.Scissors]: Choice.Paper,
+      };
+
+      if (choice === computerRandomChoice) {
+        setResult("It's a tie!");
+      } else if (winConditions[choice] === computerRandomChoice) {
+        setResult('You win this round!');
+        setUserWins(userWins + 1);
+      } else {
+        setResult('Computer wins this round!');
+        setComputerWins(computerWins + 1);
+      }
+
+      setRoundsPlayed(roundsPlayed + 1);
+
+      if (roundsPlayed === RoundsToWin) {
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }
+    }
+  };
+
+  return (
+    <div className="bg-white/5 rounded-xl w-full h-[400px] flex flex-col items-center justify-center">
+      <div className="mb-8 text-center">
+        {!gameStarted && (
+          <>
+            <p className="text-lg text-gray-300 mb-2">Welcome to Rock, Paper, Scissors!</p>
+            <p className="text-lg text-gray-300 mb-4">First to {RoundsToWin} wins the game.</p>
+            <button
+              className="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 rounded-md"
+              onClick={startGame}
+            >
+              Start Game
+            </button>
+          </>
+        )}
+        {loading && <p className="text-lg text-gray-300 mb-4">Loading...</p>}
+        {gameStarted && roundsPlayed < RoundsToWin && !loading && (
+          <>
+            <p className="text-lg text-gray-300 mb-2">Choose your weapon:</p>
+            <div className="flex space-x-4">
+              <motion.button
+                className={`text-3xl text-white hover:text-gray-300 transition duration-300 ${
+                  userChoice === Choice.Rock ? 'text-gray-300' : ''
+                }`}
+                onClick={() => handleChoice(Choice.Rock)}
+                whileHover={{ scale: 1.1 }}
+              >
+                <FaHandRock />
+              </motion.button>
+              <motion.button
+                className={`text-3xl text-white hover:text-gray-300 transition duration-300 ${
+                  userChoice === Choice.Paper ? 'text-gray-300' : ''
+                }`}
+                onClick={() => handleChoice(Choice.Paper)}
+                whileHover={{ scale: 1.1 }}
+              >
+                <FaHandPaper />
+              </motion.button>
+              <motion.button
+                className={`text-3xl text-white hover:text-gray-300 transition duration-300 ${
+                  userChoice === Choice.Scissors ? 'text-gray-300' : ''
+                }`}
+                onClick={() => handleChoice(Choice.Scissors)}
+                whileHover={{ scale: 1.1 }}
+              >
+                <FaHandScissors />
+              </motion.button>
+            </div>
+          </>
+        )}
+      </div>
+      <AnimatePresence>
+        {result && (
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            className="text-center"
+          >
+            <p className="text-2xl text-white mt-4">{result}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Comingsoon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,96 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Comingsoon from "./components/Comingsoon";
-import BlueBlur from '/Blue.png'
 import Link from "next/link";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
-
+} from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <main className="relative w-full h-screen border border-color rounded-none md:rounded-xl bg-[#011627] px-4">
       {/* NAVBAR */}
-      <div className="absolute top-0 left-0 w-full px- border-b border-color md:px-14 xl:px-32 2xl:px-60 z-40 "><Navbar /></div>
-      
-  
-  {/* CONTAINER */}
-  <section className="md:flex justify-center h-[669px] items-center text-white md:px-14 xl:px-32 2xl:px-60 pt-32 md:pt-0 gap-4 md:gap-8 xl:gap-16 mt-[49px]">
-    {/* LEFT SIDE */}
-    <div className="w-full z-10">
-      <p className="text-lg xl:text-xl z-30">What&apos;s up!, I&apos;m </p>
-
-      {/* HEADING/AVATAR WRAPPER */}
-      <div id="wrapper" className="flex items-center w-full justify-between sm:gap-4">
-        <h1 className="text-7xl md:text-7xl tracking-wide lg:tracking-tighter py-2 lg:py-0 font-base ml-[-.5rem] z-30">Kamel Singh</h1>
-        <Avatar className="w-32 h-32 lg:hidden">
-          <AvatarImage className="" src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+      <div className="absolute top-0 left-0 w-full px- border-b border-color md:px-14 xl:px-32 2xl:px-60 z-40">
+        <Navbar />
       </div>
-      <h3 className="text-xl xl:text-2xl text-[#43D9AD] z-30">&gt; full-stack developer</h3>
 
-      {/* DESKTOP SUBTITLE */}
-      <div className="pt-10 hidden md:block text-[#607B96] tracking-wide z-30 ">
-        <p className="">&gt; your future fullstack developer </p>
-        <p className=" mb-4">&gt; designing and deploying websites </p>
-        <div id="wrapper" className="flex">
-          <p className="py-1 text-[#4D5BCE]">
-            <span className="text-[#43D9AD]"><span className='text-[#607B96]'></span>const </span>contactButton <span className="text-[#607B96]">=</span>
-          </p>
-          <Button className=" z-30 cursor-pointer ml-4 text-[#fff]" variant="outline">
-            Let&apos;s talk
-          </Button>
+      {/* CONTAINER */}
+      <section className="md:flex justify-center h-[669px] items-center text-white md:px-14 xl:px-32 2xl:px-60 pt-32 md:pt-0 gap-4 md:gap-8 xl:gap-16 mt-[49px]">
+        {/* LEFT SIDE */}
+        <div className="w-full z-10">
+          <p className="text-lg xl:text-xl z-30">What&apos;s up!, I&apos;m </p>
+
+          {/* HEADING/AVATAR WRAPPER */}
+          <div id="wrapper" className="flex items-center w-full justify-between sm:gap-4">
+            <h1 className="text-7xl md:text-7xl tracking-wide lg:tracking-tighter py-2 lg:py-0 font-base ml-[-.5rem] z-30">Kamel Singh</h1>
+            <Avatar className="w-32 h-32 lg:hidden">
+              <AvatarImage className="" src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <h3 className="text-xl xl:text-2xl text-[#43D9AD] z-30">&gt; full-stack developer</h3>
+
+          {/* DESKTOP SUBTITLE */}
+          <div className="pt-10 hidden md:block text-[#607B96] tracking-wide z-30 ">
+            <p className="">&gt; your future fullstack developer </p>
+            <p className=" mb-4">&gt; designing and deploying websites </p>
+            <div id="wrapper" className="flex">
+              <p className="py-1 text-[#4D5BCE]">
+                <span className="text-[#43D9AD]"><span className="text-[#607B96]"></span>const </span>contactButton <span className="text-[#607B96]">=</span>
+              </p>
+              <Button className=" z-30 cursor-pointer ml-4 text-[#fff]" variant="outline">
+                Let&apos;s talk
+              </Button>
+            </div>
+          </div>
+
+          {/* MOBILE SUBTITLE */}
+          <div className="absolute bottom-[25%] md:hidden text-[#607B96] tracking-wide z-30">
+            <p className="py-1">&gt; your future fullstack developer </p>
+            <p className="pb-4">&gt; designing and deploying websites </p>
+            <div id="wrapper" className="flex">
+              <p className="py-1 text-[#4D5BCE]">
+                <span className="text-[#43D9AD]">const </span>contactButton <span className="text-white">=</span>
+              </p>
+              <Button className="z-30 cursor-pointer ml-2 text-[#fff]" variant="outline">
+                Let&apos;s talk
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
 
-      {/* MOBILE SUBTITLE */}
-      <div className="absolute bottom-[25%] md:hidden text-[#607B96] tracking-wide z-30">
-        <p className="py-1">&gt; your future fullstack developer </p>
-        <p className="pb-4">&gt; designing and deploying websites </p>
-        <div id="wrapper" className="flex">
-          <p className="py-1 text-[#4D5BCE]">
-            <span className="text-[#43D9AD]">const </span>contactButton <span className="text-white">=</span>
-          </p>
-          <Button className="z-30 cursor-pointer ml-2 text-[#fff]" variant="outline">
-            Let&apos;s talk
-          </Button>
+        {/* RIGHT SIDE */}
+        <div className="hidden md:flex items-center justify-center w-full pt-8 md:pt-0 z-30">
+          <Comingsoon />
         </div>
+      </section>
+
+      {/* NEXT PAGE TOOL TIP */}
+      <div className="absolute bottom-20 md:bottom-1 left-1/2 translate-x-[-50%] z-30 text-white bg-[#607B96]/20 py-1 px-[.25rem] rounded-xl cursor-pointer hover:scale-105 duration-300 ease-in-out text-[14px] md:text-base">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger><Link href={'/about'}>About Me &gt;</Link></TooltipTrigger>
+            <TooltipContent>
+              <p>check out about me</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
-    </div>
 
-    {/* RIGHT SIDE */}
-    <div className="hidden md:flex items-center justify-center w-full pt-8 md:pt-0 z-30">
-      <Comingsoon />
-    </div>
-  </section>
+      {/* FOOTER */}
+      <div className="absolute bottom-0 w-full left-0 px-2 border-t border-color md:px-14 xl:px-32 2xl:px-60 z-20 ">
+        <Footer />
+      </div>
 
-  
-  
-  {/* NEXT PAGE TOOL TIP */}
-  <div className="absolute bottom-20 md:bottom-1 left-1/2 translate-x-[-50%] z-30 text-white bg-[#607B96]/20 py-1 px-[.25rem] rounded-xl cursor-pointer hover:scale-105 duration-300 ease-in-out text-[14px] md:text-base">
-  <TooltipProvider>
-  <Tooltip>
-    <TooltipTrigger><Link href={'/about'}>About Me &gt;</Link></TooltipTrigger>
-    <TooltipContent>
-      <p>check out about me</p>
-    </TooltipContent>
-  </Tooltip>
-</TooltipProvider>
-  </div>
-
-   {/* FOOTER */}
-   <div className="absolute bottom-0 w-full left-0 px-2 border-t border-color md:px-14 xl:px-32 2xl:px-60 z-20 "><Footer /></div>
-{/* BACKGROUND BLUR IMAGES */}
-<Image src="/Green.png" fill alt="green blur" className="absolute top-0 left-0 opacity-40 z-[1]  " />
-  <Image src="/Blue.png" fill alt="blue blur" className="absolute top-0 right-0 opacity-60 z-[1]  " />
-
-</main>
-  
+      {/* BACKGROUND BLUR IMAGES */}
+      <Image src="/Green.png" fill alt="green blur" className="absolute top-0 left-0 opacity-40 z-[1]" />
+      <Image src="/Blue.png" fill alt="blue blur" className="absolute top-0 right-0 opacity-60 z-[1]" />
+    </main>
   );
 }
