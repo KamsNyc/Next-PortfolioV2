47200b3143feebc93cab69ce9b2b548d1289159f
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-11 13:50:32 -0400

    made reviewing cards scroll and responsive

--- app/components/CardScroll.tsx
+++ app/components/CardScroll.tsx
@@ -1,15 +1,9 @@
 'use client'
-
-import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Card from './Card';
 
 const CardScroll = () => {
-  const [scrollY, setScrollY] = useState(0);
 
-  const handleScroll = (event) => {
-    setScrollY(event.target.scrollTop);
-  };
 
   return (
     <section className='w-full max-h-[450px] py-4'>
@@ -20,7 +14,7 @@ const CardScroll = () => {
           scrollbarWidth: 'none',
           msOverflowStyle: 'none',
         }}
-        onScroll={handleScroll}
+
       >
         <AnimatePresence initial={false}>
           {scrollY < 1 ? (
