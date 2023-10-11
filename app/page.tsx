014fcbe333ebf3fabae0c0a937ee16c10521b6fb
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-10 20:10:22 -0400

    added review card

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import Comingsoon from "./components/Comingsoon";
 import Link from "next/link";
 import {
   Tooltip,
