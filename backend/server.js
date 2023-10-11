664d4e7f9f8146bf75d534aa644ce330b10e60f2
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-11 00:01:01 -0400

    started working on backend folder

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,42 @@ if (process.env.NODE_ENV !== 'production') {
 //import dependencies
 const express = require('express')
 const connectToDb = require('./config/connectToDb');
+const Review = require('./models/reviewModel');
 
 
 //create app
 const app = express()
 connectToDb();
 
+//config express
+app.use(express.json());
+
 
 //routing
 app.get('/', (req, res) => {
     res.json({hello: 'world'})
 });
+
+app.post('/reviews', async (req, res) => {
+    //get sent in data off request body
+    const title = req.body.title
+    const createdAt = req.body.title
+    const link = req.body.title
+    const isStar = req.body.title
+    //create a review
+
+    const review = await Review.create({
+        title: title,
+        createdAt: createdAt,
+        link: link,
+        isStar: isStar,
+    })
+
+
+
+    //respond with the new review
+    res.json
+} )
 
 
 
