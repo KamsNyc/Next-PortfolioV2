b1a3f05d9d9cc0807d99830670d3bcae0cb817e7
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-12 17:08:16 -0400

    finial work

--- app/components/AboutRightContainer.tsx
+++ app/components/AboutRightContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiFillCloseCircle, AiOutlineClose } from "react-icons/ai";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Link from "next/link";
 import Card from "./Card";
@@ -104,35 +104,97 @@ const AboutRightContainer: React.FC<AboutRightContainerProps> = ({
 
 
 
-            {/* CREATE REVIEW OPEN */}
-          <section className='relative'>   
-          {showReviewBox && (
-  <div className="fixed top-1/2 left-1/2 transform w-[300px] h-[500px] -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-[2px] p-4 rounded-md shadow-lg">
-    
+               {/* WRITE REVIEW OPEN */}
+          <section className="relative">
+            {showReviewBox && (
+              <div className="fixed top-1/2 left-1/2 transform w-[400px] h-[500px] -translate-x-1/2 -translate-y-1/2 bg-white/5 backdrop-blur-[2px] p-2 rounded-md shadow-lg">
+                {/* OPEN CONTAINER CONTENT */}
+                <section
+                  id="open__main__container"
+                  className="h-full w-full bg-[#011627] rounded-lg"
+                >
+                  {/* CONTAINER TOP MENU
+                      <div id="topmenu">
+                      <AboutItem />
+                      </div> */}
+                  {/* CONTAINER BOTTOM */}
+
+                  {/* title,
+                      body,
+                      name,
+                       date, */}
+
+                  <form className="p-4 w-full text-left h-full" action="post">
+                    {/* NAME */}
+                    <h1 className="py-2 pl-1 link-color">_name:</h1>
+                    <input
+                      type="text"
+                      name="name"
+                      required
+                      className="rounded-lg outline-[#607B96]border-none py-1 w-full bg-[#011221] text-[#465E77] pl-3"
+
+                    />
+
+                    {/* DATE */}
+                    <h1 className="py-2 pl-1 link-color">_date:</h1>
+                    <input
+                      type="date"
+                      name="date"
+                      className="rounded-lg outline-[#607B96] border-none py-1 w-full bg-[#011221] text-[#465E77] pl-3"
+                      required
+                    />
+
+                    {/* TITLE */}
+
+                    <h1 className="py-2 pl-1 link-color">_review-title:</h1>
+                    <input
+                      type="text"
+                      name="title"
+                      placeholder="please keep this short"
+                      required
+                      className="rounded-lg outline-[#607B96] border-none py-1 w-full uppercase bg-[#011221] text-[#1F6048] font-medium pl-3 placeholder:text-[#465E77]/70 placeholder:text-[14px] placeholder:lowercase"
+                    />
 
 
+                    {/* MESSAGE */}
 
+                    <h1 className="py-2 pl-1 link-color">_message:</h1>
+                    <textarea
+                      name="message"
+                      className="rounded-lg outline-[#607B96] w-full max-h-[200px] border-none py-1 min-h-[100px] pl-3 bg-[#011221] text-[#1F6048]"
+                    />
 
-     {/* CLOSE BUTTON */}
-     <button className="absolute top-0 left-0 "
-  onClick={() => {
-    setShowReviewBox(false);
-  }}
->
-  Close
-</button>
-  </div>
-)}
-
+                    {/* SUBMIT BUTTON */}
+                    <div className="pt-5">
+                      <button
+                        type="submit"
+                        className="py-2 px-2 bg-[#1C2B3A] hover:bg-[#1F6048] hover:scale-105 duration-500 ease-in-out rounded-lg text-center w-full"
+                      >
+                        submit-message
+                      </button>
+                    </div>
+                  </form>
+                </section>
+                {/* CLOSE BUTTON */}
+                <button
+                  className="absolute top-4 left-4 z-20 translate-x-[-50%] translate-y-[-50%] hover:scale-105 duration-500 ease-in-out"
+                  onClick={() => {
+                    setShowReviewBox(false);
+                  }}
+                >
+                  <AiFillCloseCircle size={40} />
+                </button>
+              </div>
+            )}
           </section>
 
-         
+
 
         </section>
-       
-          
+
+
       </section>
-      
+
     </section>
   );
 };
