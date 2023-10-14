1442fb5c0358e04da496d6f8e17e3aae617374d9
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-14 09:05:50 -0400

    fixed about

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.put("/review/:id", reviewsController.updateReview);
 app.delete("/review/:id", reviewsController.deleteReview);
 
 //start server
-
-app.listen(process.env.PORT);
+//test
+app.listen(process.env.PORT || PORT);
