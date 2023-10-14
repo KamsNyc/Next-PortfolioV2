7c2248ee3533a8b45c092359d3babf4365f559c5
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-13 22:22:48 -0400

    fixing reviews on mobile

--- app/components/AboutRightContainer.tsx
+++ app/components/AboutRightContainer.tsx
@@ -111,7 +111,7 @@ const AboutRightContainer: React.FC<AboutRightContainerProps> = ({
 
         {/*DESKTOP CONTENT */}
 
-        <section className="hidden md:flex justify-center items-center h-[670px] pt-2 px-6 text-center ">
+        <section className="block md:flex justify-center items-center h-[670px] pt-2 px-6 text-center ">
           <ScrollArea className="w-full md:mb-14">
             {/* CONTENT */}
             <AllReview />
